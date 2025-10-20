797227c8cb77ecd1208a7cb93e2e3279703157bf
ziyanaziz <ziyanazizz23@gmail.com>
2025-10-20 16:14:34 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+const navLinks = document.querySelectorAll("#menu li a");
+
+// Toggle menu show/hide
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menuIcon.style.opacity = menu.classlist .contains("active") ? "0" : "1";
+});
+
+// Close menu when any link clicked
+navLinks.forEach(link => {
+
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
