97887ded33e25c938830c88f10ca37e6a829f1ed
ziyanaziz <ziyanazizz23@gmail.com>
2025-10-31 21:45:24 +0500

    Updated portfolio designt

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const navLinks = document.querySelectorAll("#menu li a");
 // Toggle menu show/hide
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("active");
-  menuIcon.style.opacity = menu.classlist .contains("active") ? "0" : "1";
+  menuIcon.style.opacity = menu.classlist.contains("active") ? "0" : "1";
+
 });
 
 // Close menu when any link clicked
 navLinks.forEach(link => {
-
   link.addEventListener("click", () => {
     menu.classList.remove("active");
   });
